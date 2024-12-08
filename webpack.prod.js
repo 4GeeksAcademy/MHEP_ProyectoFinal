77b1fc3f5a073b93e59cbcb3560c966a7f82ab53
ElioPeraza <157733983+ElioPeraza@users.noreply.github.com>
2024-12-08 21:02:52 +0000

    changes for versel

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        publicPath: '/'
+        publicPath: '/', // Ruta base
     },
     plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+        // No incluyas dotenv-webpack si no necesitas variables de entorno
+    ],
 });
+
